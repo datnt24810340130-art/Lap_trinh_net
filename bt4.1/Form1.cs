543be9193b6c8bbc37a0839c055cf28c732c6d48
Nguyen Thanh Dat <datnt.24810340130@epu.edu.vn>
2026-09-23 14:53:36 +0700

    bai4.1

---
+++ bt4.1/Form1.cs
@@ -0,0 +1,15 @@
+namespace bt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
