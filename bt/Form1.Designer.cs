836334a571a69d8af404823bf7938de307fead4f
Nguyen Thanh Dat <datnt.24810340130@epu.edu.vn>
2026-09-23 14:50:14 +0700

    bai4.1

---
+++ bt/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace bt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lbl = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            lblPassword = new Label();
+            txtUsername = new TextBox();
+            textBox1 = new TextBox();
+            chkShowPassword = new CheckBox();
+            btnLogin = new Button();
+            btnExit = new Button();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // lbl
+            // 
+            lbl.AutoSize = true;
+            lbl.Location = new Point(144, 69);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(110, 20);
+            lbl.TabIndex = 0;
+            lbl.Text = "Tên đăng nhập:";
+            lbl.Click += lbl_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(86, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 20);
+            label2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(144, 107);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 0;
+            label1.Click += lbl_Click;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(150, 121);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(75, 20);
+            lblPassword.TabIndex = 0;
+            lblPassword.Text = "Mật Khẩu:";
+            lblPassword.Click += lbl_Click;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(270, 66);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(189, 27);
+            txtUsername.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(270, 121);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(189, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // chkShowPassword
+            // 
+            chkShowPassword.AutoSize = true;
+            chkShowPassword.Location = new Point(481, 124);
+            chkShowPassword.Name = "chkShowPassword";
+            chkShowPassword.Size = new Size(127, 24);
+            chkShowPassword.TabIndex = 4;
+            chkShowPassword.Text = "Hiện mật khẩu";
+            chkShowPassword.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(244, 181);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Đăng nhập";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(370, 181);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(94, 29);
+            btnExit.TabIndex = 5;
+            btnExit.Text = "Thoát";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnExit);
+            Controls.Add(btnLogin);
+            Controls.Add(chkShowPassword);
+            Controls.Add(textBox1);
+            Controls.Add(txtUsername);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lblPassword);
+            Controls.Add(lbl);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbl;
+        private Label label2;
+        private Label label1;
+        private Label lblPassword;
+        private TextBox txtUsername;
+        private TextBox textBox1;
+        private CheckBox chkShowPassword;
+        private Button btnLogin;
+        private Button btnExit;
+        private ErrorProvider errorProvider1;
+    }
+}
